7466c7da5bd6136e5f72b08879b32d988c478a50
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-01-04 12:26:03 +0100

    - backported ALSA init fix.

--- source/zmusic/configuration.cpp
+++ source/zmusic/configuration.cpp
@@ -190,7 +190,8 @@ struct MidiDeviceList
 #ifdef HAVE_SYSTEM_MIDI
 #ifdef __linux__
 		auto& sequencer = AlsaSequencer::Get();
-		auto& dev = sequencer.GetDevices();
+		sequencer.EnumerateDevices();
+		auto& dev = sequencer.GetInternalDevices();
 		for (auto& d : dev)
 		{
 			MidiOutDevice mdev = { strdup(d.Name.c_str()), d.ID, MIDIDEV_MAPPER };	// fixme: Correctly determine the type of the device.
