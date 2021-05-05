0f1d954646d272473d1c08910634bfda45abc663
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-05-05 11:17:45 +0300

    - avoid passing nullptr args to MIDI streamer
    https://forum.zdoom.org/viewtopic.php?t=72161

--- source/musicformats/music_midi.cpp
+++ source/musicformats/music_midi.cpp
@@ -1021,7 +1021,7 @@ DLL_EXPORT zmusic_bool ZMusic_MIDIDumpWave(ZMusic_MidiSource source, EMidiDevice
 {
 	try
 	{
-		MIDIStreamer me(devtype, devarg);
+		MIDIStreamer me(devtype, devarg ? devarg : "");
 		me.SetMIDISource(source);
 		me.DumpWave(outname, subsong, samplerate);
 		return true;
